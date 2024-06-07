8322cb69af875f9eec79de475267be6093c57daa
Anh Nguyen <116730429+AnhKrenius@users.noreply.github.com>
2024-06-07 17:24:51 +1000

    Update app2.py

--- app2.py
+++ app2.py
@@ -35,7 +35,7 @@ def fetch_submissions(subreddit_name, keyword, sort_types):
             except Exception as e:
                 st.warning(f"Error fetching submissions for sort type {sort_type}: {e}")
     return combined_submissions
-def draw_plot_for_keyword(keyword,subreddit_name):
+def create_dataframe(keyword,subreddit_name):
     sort_types = ['relevance','hot','top','new','comments']
     combined_submissions = fetch_submissions(subreddit_name, keyword, sort_types)
     sorted_submissions = sorted(combined_submissions.values(), key=lambda x:x.created_utc,reverse = True )
@@ -94,6 +94,9 @@ def draw_plot_for_keyword(keyword,subreddit_name):
         else:
             return 'Not classified'
     df['sentiment'] = df.apply(get_sentiment, axis = 1)
+    return df
+  
+def draw_pie_chart(df):  
     sentiment_counts = df['sentiment'].value_counts()
     plt.figure(figsize=(8,6))
     plt.pie(sentiment_counts, labels = sentiment_counts.index, autopct ='%1.1f%%', startangle = 140)
@@ -102,8 +105,7 @@ def draw_plot_for_keyword(keyword,subreddit_name):
         os.makedirs('static')
     plt.savefig('static/sentiment_pie_chart.png')
     plt.close()
-
-    return df, 'static/sentiment_pie_chart.png'
+    return 'static/sentiment_pie_chart.png'
 
 def draw_wordcloud(df):
     #wordcloud
@@ -120,10 +122,10 @@ def draw_wordcloud(df):
 
     return 'wordcloud.png'
 # Sample posts
-def get_sample_posts(df):
-  pos_posts = df[(df['sentiment']=='POS')&(df['isself']==True)].sample(3)
-  neg_posts = df[(df['sentiment']=='NEG')&(df['isself']==True)].sample(3)
-  neu_posts = df[(df['sentiment']=='NEU')&(df['isself']==True)].sample(3)
+def get_posts(df):
+  pos_posts = df[df['sentiment']=='POS']
+  neg_posts = df[df['sentiment']=='NEG']
+  neu_posts = df[df['sentiment']=='NEU']
   return pos_posts, neg_posts, neu_posts
 #Streamlit app
 page_bg_img = """
@@ -169,40 +171,73 @@ subreddit = st.text_input('Enter a subreddit (optional):','all')
 
 if st.button('Search'):
     with st.spinner('Fetching and analyzing data...'):
-        df, pie_chart_path = draw_plot_for_keyword(keyword,subreddit)
+        df = create_dataframe(keyword,subreddit)
         st.write(f"Total: {len(df)} posts")
-        wordcloud_path = draw_wordcloud(df)
+        min_year = int(df['created_year'].min())
+        max_year = int(df['created_year'].max())
+        chosen_min_year, chosen_max_year = st.slider('Choose a Year Range', min_value=min_year, max_value = max_year, value = (min_year, max_year))
+        df_sub = df[(df['created_year']<=chosen_max_year)&(df['created_year']>=chosen_min_year)]
+        pie_chart_path = draw_pie_chart(df_sub)
+        wordcloud_path = draw_wordcloud(df_sub)
         images = [pie_chart_path, wordcloud_path]
         st.image(images,width=468)
-        st.header("Sample posts")
-        pos_posts, neg_posts, neu_posts = get_sample_posts(df)
-        col1, col2, col3 = st.columns(3,gap = "medium")
-        def short(text1):
-          try:
-            text = text1[:70]
-          except:
-            text = text1
-          return text
-        def short_title(title1):
-          try:
-            title = title1[:20]
-          except:
-            title = title1
-          return title
+        pos_posts, neg_posts, neu_posts = get_posts(df_sub)
+        note = '<p style="font-family:Fira Sans; font-size: 16px;"><i>👉 Click on Score to sort as desired</i></p>'
+        st.markdown(note, unsafe_allow_html=True)
+        st.markdown('\n') # see #*
+        col1,col2,col3=st.columns(3,gap='medium')
         with col1:
-            st.subheader("😊"+short_title(pos_posts.iloc[0]["Title"]))
-            st.markdown(short(pos_posts.iloc[0]["Text"])+'...')
-            url1 = pos_posts.iloc[0]["URL"]
-            st.write("Read more [link](%s)" % url1)
+            st.dataframe(pos_posts[['Title','score','URL']],height =1000,hide_index=True,
+                         column_config={
+                             "Title": st.column_config.TextColumn(
+                                 "😊 Positive Submissions",
+                                 width=250
+                             ),
+                             "score": st.column_config.NumberColumn(
+                                 "Score",
+                                 help='A submission \'s score is simply the number of upvotes minus the number of downvotes.',
+                                 format= "%d ⭐",
+                                 width=80
+                             ),
+                             "URL": st.column_config.LinkColumn(
+                                 "url",
+                                 width=40
+                             )
+                             })
         with col2:
-            st.subheader("😭"+short_title(neg_posts.iloc[0]["Title"]))
-            st.markdown(short(neg_posts.iloc[0]["Text"])+'...')
-            url2 = neg_posts.iloc[0]["URL"]
-            st.write("Read more [link](%s)" % url2)
+            st.dataframe(neg_posts[['Title','score','URL']],height =1000,hide_index=True,
+                         column_config={
+                             "Title": st.column_config.TextColumn(
+                                 "😭 Negative Submissions",
+                                 width=250
+                             ),
+                             "score": st.column_config.NumberColumn(
+                                 "Score",
+                                 help='A submission \'s score is simply the number of upvotes minus the number of downvotes.',
+                                 format= "%d ⭐",
+                                 width=80
+                             ),
+                             "URL": st.column_config.LinkColumn(
+                                 "url",
+                                 width=40
+                             )
+                             })
         with col3:
-            st.subheader("😶"+short_title(neu_posts.iloc[0]["Title"]))
-            st.markdown(short(neu_posts.iloc[0]["Text"]) + '...')
-            url3 = neu_posts.iloc[0]["URL"]
-            st.write("Read more [link](%s)" % url3)
-
-
+            st.dataframe(neu_posts[['Title','score','URL']],height=1000,hide_index=True,
+                         column_config={
+                             "Title": st.column_config.TextColumn(
+                                 "😶 Neutral Submissions",
+                                 width=250
+                             ),
+                             "score": st.column_config.NumberColumn(
+                                 "Score",
+                                 help = 'A submission \'s score is simply the number of upvotes minus the number of downvotes.',
+                                 format = "%d ⭐",
+                                 width=80
+                             ),
+                             'URL': st.column_config.LinkColumn(
+                                 "url", width=40
+                             )
+                         })
+        
+        
