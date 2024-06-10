4961c9bd12d246de7530baa3f7bb0d5bc5a73b8b
Anh Nguyen <116730429+AnhKrenius@users.noreply.github.com>
2024-06-10 13:46:15 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,87 +4,233 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from transformers import pipeline
 from datetime import datetime
+from wordcloud import WordCloud
+from concurrent.futures import ThreadPoolExecutor
 import os
-from config import reddit_config
+# from config import reddit_config
 # Initialize Reddit instance
-reddit = praw.Reddit(client_id=reddit_config['client_id'],
-                     client_secret=reddit_config['client_secret'],
-                     user_agent=reddit_config['user_agent'],
-                     username=reddit_config['username'],
-                     password=reddit_config['password'])
-
-# Function to perform the search
+reddit = praw.Reddit(client_id=st.secrets["client_id"],
+                     client_secret=st.secrets["client_secret"],
+                     user_agent='testscript by u/sentiment',
+                     username=st.secrets["username"],
+                     password=st.secrets["password"])
+#Function to perform the search
 def perform_search(subreddit_name, keyword, sort):
     if subreddit_name == "all":
-        submissions = reddit.subreddit("all").search(keyword, sort=sort, limit=100)
+        submissions = reddit.subreddit("all").search(keyword,sort=sort,limit=1000)
     else:
         subreddit = reddit.subreddit(subreddit_name)
-        submissions = subreddit.search(keyword, sort=sort, limit=100)
+        submissions = subreddit.search(keyword,sort=sort, limit = 1000)
     return submissions
-
-def draw_plot_for_keyword(keyword, subreddit_name="all"):
-    sort_types = ['relevance', 'hot', 'top', 'new', 'comments']
+def fetch_submissions(subreddit_name, keyword, sort_types):
     combined_submissions = {}
-
-    for sort_type in sort_types:
-        submissions = perform_search(subreddit_name, keyword, sort=sort_type)
-        for submission in submissions:
-            combined_submissions[submission.id] = submission
-
-    sorted_submissions = sorted(combined_submissions.values(), key=lambda x: x.created_utc, reverse=True)
-
-    df = pd.DataFrame([{
+    with ThreadPoolExecutor() as executor:
+        futures = {executor.submit(perform_search, subreddit_name, keyword, sort): sort for sort in sort_types}
+        for future in futures:
+            sort_type = futures[future]
+            try:
+                submissions = future.result()
+                for submission in submissions:
+                    combined_submissions[submission.id] = submission
+            except Exception as e:
+                st.warning(f"Error fetching submissions for sort type {sort_type}: {e}")
+    return combined_submissions
+sentiment_classifier = pipeline('sentiment-analysis', model='distilbert-base-uncased-finetuned-sst-2-english')
+def create_dataframe(keyword,subreddit_name):
+    sort_types = ['relevance','hot','top','new','comments']
+    combined_submissions = fetch_submissions(subreddit_name, keyword, sort_types)
+    data = []
+    for submission in combined_submissions:
+      try:
+        # Ensure consistent datetime conversion
+        submission_time = datetime.utcfromtimestamp(submission.created_utc).strftime('%d-%m-%Y %H:%M:%S')
+      except Exception as e:
+        submission_time = ''
+    
+      data.append({
         'ID': submission.id,
         'Subreddit': submission.subreddit.display_name,
         'Title': submission.title,
         'Text': submission.selftext,
-        'URL': submission.url,
-        'Time': datetime.utcfromtimestamp(submission.created_utc).strftime('%d-%m-%Y %H:%M:%S')
-    } for submission in sorted_submissions])
+        'Permalink': submission.permalink,
+        'Time': submission_time,
+        'score':submission.score if submission.score else 0
+      })
 
-    df['Time'] = pd.to_datetime(df['Time'])
+# Create DataFrame
+    df = pd.DataFrame(data)
+    df['URL']='https://www.reddit.com/' + df['Permalink']
+    df['Time'] = pd.to_datetime(df['Time'], errors ='coerce')
     df['created_year'] = df['Time'].dt.year
+    def get_sentiment(row):
+        # create a list to store chunks of text
+        text_chunks = []
 
-    sentiment_classifier = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
-
-    def get_sentiment(text):
-        try:
-            sentiment = sentiment_classifier(text)[0]['label']
-        except:
+        if pd.notnull(row['Text']):  # if 'Text' is not null, tokenize it
+            text_chunks = [row['Text'][i:i + 512] for i in range(0, len(row['Text']), 512)]
             try:
-                text_chunks = [text[i:i+512] for i in range(0, len(text), 512)]
-                sentiment_results = [sentiment_classifier(chunk)[0]['label'] for chunk in text_chunks]
-                sentiment = max(set(sentiment_results), key=sentiment_results.count)
+                sentiment_results = sentiment_classifier(text_chunks)
             except:
-                sentiment = "Not Classified"
-        return sentiment
-
-    df.loc[df['Text'].isna(), 'Text'] = df.loc[df['Text'].isna(), 'Title']
-    df['sentiment'] = df['Text'].apply(lambda x: get_sentiment(x))
-
+                sentiment_results = []
+            sentiments = [result['label'] for result in sentiment_results if 'label' in result]
+            # get the most common sentiment
+            common_sentiment = max(set(sentiments), key=sentiments.count) if sentiments else "Not classified"
+            if common_sentiment == "Not classified":  # Duty cycle for not classified sentiment
+                return get_sentiment({"Text": None, "Title": row['Title']})
+            else:
+                return common_sentiment
+        elif pd.notnull(row['Title']):  # if 'Text' is null, tokenize 'Title'
+            text_chunks = [row['Title'][i:i + 512] for i in range(0, len(row['Title']), 512)]
+            try:
+                sentiment_results = sentiment_classifier(text_chunks)
+            except:
+                return 'Not classified'
+            sentiments = [result['label'] for result in sentiment_results if 'label' in result]
+            # get the most common sentiment
+            common_sentiment = max(set(sentiments), key=sentiments.count) if sentiments else "Not classified"
+            return common_sentiment
+        else:
+            return 'Not classified'
+    df['sentiment'] = df.apply(get_sentiment, axis = 1)
+    return df
+  
+def draw_pie_chart(df):  
     sentiment_counts = df['sentiment'].value_counts()
-
-    plt.figure(figsize=(8, 6))
-    plt.pie(sentiment_counts, labels=sentiment_counts.index, autopct='%1.1f%%', startangle=140)
+    plt.figure(figsize=(8,6))
+    plt.pie(sentiment_counts, labels = sentiment_counts.index, autopct ='%1.1f%%', startangle = 140)
     plt.title(f'Sentiment Distribution for "{keyword}"')
-
     if not os.path.exists('static'):
         os.makedirs('static')
     plt.savefig('static/sentiment_pie_chart.png')
     plt.close()
-
     return 'static/sentiment_pie_chart.png'
 
-# Streamlit app
-st.title('Reddit Sentiment Analysis')
+def draw_wordcloud(df):
+    #wordcloud
+    post_title_text = ' '.join([title for title in df['Title'].str.lower()])
+    word_cloud = WordCloud(collocation_threshold = 2, width = 1000, height=500,
+                       background_color='white').generate(post_title_text)
+    #Display
+    plt.figure(figsize=(8,6))
+    plt.imshow(word_cloud)
+    plt.axis("off")
+    plt.title(f'Word Cloud')
+    plt.savefig(f'wordcloud.png')
+    plt.close()
+
+    return 'wordcloud.png'
+# Sample posts
+def get_posts(df):
+  pos_posts = df[df['sentiment']=='POS']
+  neg_posts = df[df['sentiment']=='NEG']
+  neu_posts = df[df['sentiment']=='NEU']
+  return pos_posts, neg_posts, neu_posts
+#Streamlit app
+page_bg_img = """
+<style>
+section.main >div {max-width:90rem}
+[data-testid="stAppViewContainer"] {
+background-image: url("https://i.pinimg.com/originals/e4/52/99/e45299d660b601f029fe173f084feb42.jpg");
+background-size: cover;
+}
+[data-testid="stHeader"]{
+background:rgba(0,0,0,0);
+}
+[data-testid="stImage"]{
+border: 1px solid #ddd;
+border-radius: 4px;
+padding: 5px;
+}
+</style>
+"""
+st.set_page_config(layout = "wide",
+                   page_icon="☕",
+                   page_title="GuLu")
+st.markdown(page_bg_img, unsafe_allow_html=True)
+st.title("Social Media Sentiment Analysis")
 st.write("Analyze the sentiment of Reddit posts based on a given keyword.")
-
-keyword = st.text_input('Enter a keyword:')
-subreddit = st.text_input('Enter a subreddit (optional):', 'all')
-
-if st.button('Search'):
-    with st.spinner('Fetching and analyzing data...'):
-        image_path = draw_plot_for_keyword(keyword, subreddit)
-        st.image(image_path)
-        st.success('Analysis complete!')
-
+keyword = st.text_input('Enter a keyword: ')
+subreddit = st.text_input('Enter a subreddit (optional):','all')
+if 'data_loaded' not in st.session_state:
+  st.session_state.data_loaded = False
+if not st.session_state.data_loaded:
+  if st.button('Search'):
+    with st.spinner('Collecting data...'):
+        df = create_dataframe(keyword,subreddit)
+        st.session_state.data_loaded = True
+        st.session_state.df = df
+if st.session_state.data_loaded:
+    with st.spinner('Analyzing data...'):
+        df=st.session_state.df
+        if st.button('Search other keyword'):
+          st.session_state.data_loaded = False
+          st.rerun()
+        st.write(f"Total: {len(df)} posts")
+        min_year = int(df['created_year'].min())
+        max_year = int(df['created_year'].max())
+        chosen_min_year, chosen_max_year = st.slider('Choose a Year Range', min_value=min_year, max_value = max_year, value = (min_year, max_year))
+        df_sub = df[(df['created_year']<=chosen_max_year)&(df['created_year']>=chosen_min_year)]
+        pie_chart_path = draw_pie_chart(df_sub)
+        wordcloud_path = draw_wordcloud(df_sub)
+        images = [pie_chart_path, wordcloud_path]
+        st.image(images,width=468)
+        pos_posts, neg_posts, neu_posts = get_posts(df_sub)
+        note = '<p style="font-family:Fira Sans; font-size: 16px;"><i>👉 Click on Score to sort as desired</i></p>'
+        st.markdown(note, unsafe_allow_html=True)
+        st.markdown('\n') # see #*
+        col1,col2,col3=st.columns(3,gap='medium')
+        with col1:
+            st.dataframe(pos_posts[['Title','score','URL']],height =1000,hide_index=True,
+                         column_config={
+                             "Title": st.column_config.TextColumn(
+                                 "😊 Positive Submissions",
+                                 width=250
+                             ),
+                             "score": st.column_config.NumberColumn(
+                                 "Score",
+                                 help='A submission \'s score is simply the number of upvotes minus the number of downvotes.',
+                                 format= "%d ⭐",
+                                 width=80
+                             ),
+                             "URL": st.column_config.LinkColumn(
+                                 "url",
+                                 width=40
+                             )
+                             })
+        with col2:
+            st.dataframe(neg_posts[['Title','score','URL']],height =1000,hide_index=True,
+                         column_config={
+                             "Title": st.column_config.TextColumn(
+                                 "😭 Negative Submissions",
+                                 width=250
+                             ),
+                             "score": st.column_config.NumberColumn(
+                                 "Score",
+                                 help='A submission \'s score is simply the number of upvotes minus the number of downvotes.',
+                                 format= "%d ⭐",
+                                 width=80
+                             ),
+                             "URL": st.column_config.LinkColumn(
+                                 "url",
+                                 width=40
+                             )
+                             })
+        with col3:
+            st.dataframe(neu_posts[['Title','score','URL']],height=1000,hide_index=True,
+                         column_config={
+                             "Title": st.column_config.TextColumn(
+                                 "😶 Neutral Submissions",
+                                 width=250
+                             ),
+                             "score": st.column_config.NumberColumn(
+                                 "Score",
+                                 help = 'A submission \'s score is simply the number of upvotes minus the number of downvotes.',
+                                 format = "%d ⭐",
+                                 width=80
+                             ),
+                             'URL': st.column_config.LinkColumn(
+                                 "url", width=40
+                             )
+                         })
+        
+        
