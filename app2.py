b338272a16d6fdfe2ce30491ecc472743966854b
Anh Nguyen <116730429+AnhKrenius@users.noreply.github.com>
2024-06-17 04:53:12 +1000

    Update app2.py

--- app2.py
+++ app2.py
@@ -8,11 +8,11 @@ from wordcloud import WordCloud
 from concurrent.futures import ThreadPoolExecutor
 import os
 # Initialize Reddit instance
-reddit = praw.Reddit(client_id='yMQwdMYVS1J3wVfeb_3fuw',
-                     client_secret='3sFTR2aigu8d0D2rXMFkNxJpyM3KLQ',
+reddit = praw.Reddit(client_id=st.scecrets['client_id'],
+                     client_secret=st.scecrets['client_secret'],
                      user_agent='testscript by u/sentiment',
-                     username='Gulugulugulu1607',
-                     password='gulugulugulu')
+                     username=st.scecrets['username'],
+                     password=st.scecrets['password'])
 #Function to perform the search
 def perform_search(subreddit_name, keyword, sort):
     if subreddit_name == "all":
