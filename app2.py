ccba0ac4f08cda64b92be56489c79fa616cd594c
Anh Nguyen <116730429+AnhKrenius@users.noreply.github.com>
2024-06-30 15:28:44 +1000

    Update app2.py

--- app2.py
+++ app2.py
@@ -13,7 +13,7 @@ reddit = praw.Reddit(client_id=os.getenv('h_client_id'),
                      client_secret=os.getenv('h_client_secret'),
                      user_agent='testscript by u/sentiment',
                      username=os.getenv('h_username'),
-                     password=os.getenv('h_password')
+                     password=os.getenv('h_password'))
 #Function to perform the search
 def perform_search(subreddit_name, keyword, sort):
     if subreddit_name == "all":
